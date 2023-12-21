be9eadea4206e7ee3f57ccd23885f504ba868676
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-12-21 11:22:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let row = 3 ;
+let table = document.getElementById("sampleTable");
 function insert_Row() {
     //Write your code here
-  
-  
+	let newRow = document.createElement("tr");
+	let cell1 = document.createElement("td");
+	let cell2 = document.createElement("td");
+	cell1.innerText = `Row${row} cell1`;
+	cell2.innerText = `Row${row} cell1`;
+	row++;
+	newRow.append(cell1,cell2);
+	table.appendChild(newRow);
 }
